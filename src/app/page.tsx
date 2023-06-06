f83262cc8b095f0733d07f92ecd836e78207e824
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-07 04:16:00 +0530

    completed-basic-todo-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 
 import React, { useState } from 'react';
 import { TodoObject } from './models/Todo';
+import { v4 as uuid } from 'uuid';
 const Home: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<TodoObject[]>([]);
   const addTodo = () => {
-    setTodos([{ id: '1', value: todo, done: false }, ...todos]);
+    setTodos([{ id: uuid(), value: todo, done: false }, ...todos]);
     setTodo('');
+  };
+
+  const markTodoDone = (id: string) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
   };
   return (
     <>
@@ -27,7 +36,13 @@ const Home: React.FC = () => {
         </button>
         <ul className="mt-5 ">
           {todos.map((todo) => (
-            <li key={todo.id} className="text-3xl ml-5 cursor-pointer">
+            <li
+              key={todo.id}
+              className={`text-3xl ml-5 cursor-pointer ${
+                todo.done ? 'line-through' : 'no-underline'
+              }`}
+              onClick={() => markTodoDone(todo.id)}
+            >
               {todo.value}
             </li>
           ))}
